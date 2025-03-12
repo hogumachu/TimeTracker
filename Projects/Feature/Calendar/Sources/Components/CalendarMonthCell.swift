5b2e21bbabbf62b93f12eb1c2db925eedc816ab4
sungjun-hong-ikoob <74225754+hogumachu@users.noreply.github.com>
2025-03-12 16:58:34 +0900

    wip

--- Projects/Feature/Calendar/Sources/Components/CalendarMonthCell.swift
+++ Projects/Feature/Calendar/Sources/Components/CalendarMonthCell.swift
@@ -1,5 +1,5 @@
 //
-//  CalendarMonthView.swift
+//  CalendarMonthCell.swift
 //  Calendar
 //
 //  Created by 홍성준 on 3/11/25.
@@ -14,15 +14,34 @@ import FlexLayout
 import PinLayout
 import Then
 
-final class CalendarMonthView: BaseView {
+final class CalendarMonthCell: UICollectionViewCell {
   
   private enum Constants {
     static let range = 0..<6
   }
   
-  override func setupLayout() {
-    super.setupLayout()
-    rootView.flex
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupLayout()
+  }
+  
+  @available(*, unavailable)
+  required public init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func configure(_ color: UIColor) {
+    contentView.backgroundColor = color
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    contentView.backgroundColor = .clear
+  }
+  
+  private func setupLayout() {
+    contentView.backgroundColor = DesignSystem.BackgroundColor.base
+    contentView.flex
       .define { flex in
         Constants.range.forEach { _ in
           flex.addItem(CalendarMonthWeekView())
@@ -32,8 +51,13 @@ final class CalendarMonthView: BaseView {
   
   override func layoutSubviews() {
     super.layoutSubviews()
-    rootView.pin.all()
-    rootView.flex.layout(mode: .adjustHeight)
+    contentView.flex.layout(mode: .adjustHeight)
+  }
+  
+  override func sizeThatFits(_ size: CGSize) -> CGSize {
+    contentView.pin.width(size.width)
+    contentView.flex.layout(mode: .adjustHeight)
+    return contentView.frame.size
   }
 }
 
--- Projects/Feature/Calendar/Sources/Components/CalendarView.swift
+++ Projects/Feature/Calendar/Sources/Components/CalendarView.swift
@@ -20,7 +20,7 @@ final class CalendarView: BaseView {
     $0.backgroundColor = DesignSystem.BackgroundColor.groupedBase
   }
   private let headerView = CalendarMonthHeaderView()
-  private let contentView = CalendarMonthView()
+  private let contentView = CalendarMonthContainerView()
   
   override func setupLayout() {
     addSubview(containerView)
@@ -44,5 +44,96 @@ final class CalendarView: BaseView {
     containerView.pin.all()
     rootView.pin.top(pin.safeArea).horizontally().bottom()
     rootView.flex.layout()
+  }
+}
+
+final class CalendarMonthContainerView: UIView {
+  
+  private enum Constants {
+    static let cellWidth = UIScreen.main.bounds.width
+  }
+  
+  private var items = (1...3)
+    .map { _ in [UIColor.gray, .red, .blue, .orange, .black].randomElement()! }
+  
+  private let collectionViewFlowLayout: UICollectionViewFlowLayout = {
+    let layout = UICollectionViewFlowLayout()
+    layout.scrollDirection = .horizontal
+    layout.itemSize = .init(width: Constants.cellWidth, height: UIScreen.main.bounds.height)
+    layout.minimumLineSpacing = 0
+    layout.minimumInteritemSpacing = 0
+    return layout
+  }()
+  
+  private lazy var collectionView: UICollectionView = {
+    let view = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+    view.isScrollEnabled = true
+    view.showsHorizontalScrollIndicator = false
+    view.showsVerticalScrollIndicator = true
+    view.contentInset = .zero
+    view.clipsToBounds = true
+    view.register(CalendarMonthCell.self)
+    view.isPagingEnabled = true
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupViews()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupViews() {
+    items.insert(items[items.count - 1], at: 0)
+    items.append(items[1])
+    
+    addSubview(collectionView)
+    NSLayoutConstraint.activate([
+      collectionView.topAnchor.constraint(equalTo: topAnchor),
+      collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+      collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+      collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+    ])
+    
+    collectionView.dataSource = self
+    collectionView.delegate = self
+  }
+  
+}
+
+extension CalendarMonthContainerView: UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    items.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueCell(type: CalendarMonthCell.self, for: indexPath)
+    cell.configure(items[indexPath.row])
+    return cell
+  }
+}
+
+extension CalendarMonthContainerView: UICollectionViewDelegate {
+  func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    let count = items.count
+    
+    if scrollView.contentOffset.x == 0 {
+      scrollView.setContentOffset(
+        .init(x: Constants.cellWidth * Double(count - 2), y: scrollView.contentOffset.y),
+        animated: false
+      )
+    }
+    
+    if scrollView.contentOffset.x == Double(count - 1) * Constants.cellWidth {
+      scrollView.setContentOffset(
+        .init(x: Constants.cellWidth, y: scrollView.contentOffset.y),
+        animated: false
+      )
+    }
   }
 }
